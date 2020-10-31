c79bfafa806c43a4316e257c7a9855ae463cd36e
Hanspln <hanspln@hey.com>
2020-10-31 07:30:47 +0000

    Added comment to Gemfil to see if everything works automatically now

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_10_30_050136) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "friendly_id_slugs", force: :cascade do |t|
     t.string "slug", null: false
     t.integer "sluggable_id", null: false
