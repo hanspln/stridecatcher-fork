a10fb6b68d7dd4159d2e2fceead0686b24d229c4
Hanspln <hanspln@hey.com>
2020-11-01 08:36:31 +0000

    Setting up system tests with firefox headless. Working

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,15 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
+  driven_by :selenium, using: :headless_firefox, screen_size: [1200, 600]
 end
+
+#Capybara.register_driver :headless_chrome do |app|
+# capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+#   chromeOptions: { args: %w(headless disable-gpu) }
+# )
+#
+# Capybara::Selenium::Driver.new app,
+#   browser: :chrome,
+#   desired_capabilities: capabilities
+#end
