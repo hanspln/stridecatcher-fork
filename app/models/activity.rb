00831725d5e6919ed0ba5d95d2d54de3927fac1e
Hanspln <hanspln@hey.com>
2020-11-03 06:25:03 +0000

    add enums to activities, associate with user

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,2 +1,8 @@
 class Activity < ApplicationRecord
+  belongs_to :user
+  
+  enum category: [:run, :long_run, :workout, :race, :other]
+  enum difficulty: [:easy, :moderate, :hard]
+  enum unit: [:miles, :kilometers, :meters, :yards]
+  enum unit: [:miles, :kilometers, :meters, :yards]
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-has_person_name
 
+  has_many :activities, dependent: :destroy
+
+  has_person_name
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
